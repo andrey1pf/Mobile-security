b6d471e5e3c400c577c5c7a19f34caa55283581e
andreypf_1 <goncharenkoandrey8@gmail.com>
2023-04-12 21:58:22 +0300

    add user check for windows

--- lab2/appController/__init__.py
+++ lab2/appController/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import os
 import sys
+import ctypes
+
+from appController.controllers import system_check
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -8,8 +11,14 @@ app.config.update(
     DEBUG=True
 )
 
-if os.geteuid() == 0:
-    print("Error: The application must not be run with administrator/root")
-    sys.exit(1)
+system = system_check.platform_check()
+if system == 'Windows':
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        print("Error: The application must not be run with administrator/root")
+        sys.exit(1)
+else:
+    if os.geteuid() == 0:
+        print("Error: The application must not be run with administrator/root")
+        sys.exit(1)
 
 from appController import routes
